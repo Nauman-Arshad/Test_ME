c1df4e82e8004fdb352e034170440fb63bc6c6c6
Rao Akif <akifrao@gmail.com>
2021-01-25 19:18:36 +0500

    React Helmet Seemed Added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,14 @@
 import React from "react"
+import { Helmet } from "react-helmet"
 
 export default function HomePage() {
   return (
     <div>
+      <Helmet>
+          <meta charSet="utf-8" />
+          <title>GatsbyPWA</title>
+          <link rel="canonical" href="https://testcurrent.netlify.app" />
+        </Helmet>
       <h1>Hello World</h1>
     </div>
   )
