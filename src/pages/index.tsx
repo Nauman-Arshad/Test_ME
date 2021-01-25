0e897a7d7db13422391f13c1cd05efbabb9e50d2
Rao Akif <akifrao@gmail.com>
2021-01-25 20:04:29 +0500

    All additions deleted

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,8 @@
 import React from "react"
-// import { Helmet } from "react-helmet"
 
 export default function HomePage() {
   return (
     <div>
-      {/* <Helmet>
-          <meta charSet="utf-8" />
-          <title>Gatsby PWA</title>
-          <link rel="canonical" href="https://testcurrent.netlify.app" />
-        </Helmet> */}
       <h1>Hello World</h1>
     </div>
   )
