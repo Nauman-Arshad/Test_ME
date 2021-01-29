678091854326fcc58a3c9b92c4ae9faa84f4ad3a
Rao Akif <akifrao@gmail.com>
2021-01-29 21:18:13 +0500

    div added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,64 +1,62 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from 'react';
 import { useQuery, useMutation } from '@apollo/client';
 import gql from 'graphql-tag';
-import "./style.css";
+import './style.css';
 import Card from '../components/card';
 
 const GET_BOOKMARKS = gql`
-{
+  {
     bookmarks {
-        id
-        url
-        title
+      id
+      url
+      title
     }
-}
+  }
 `;
 
 const ADD_BOOKMARK = gql`
-    mutation addBookmar($url: String!, $title: String!){
-        addBookmark(url: $url, title: $title){
-            id
-        }
+  mutation addBookmar($url: String!, $title: String!) {
+    addBookmark(url: $url, title: $title) {
+      id
     }
-`
+  }
+`;
 
 export default function Home() {
+  let titleField;
+  let urlField;
 
-    let titleField;
-    let urlField;
+  const { error, loading, data } = useQuery(GET_BOOKMARKS);
+  const [addBookmark] = useMutation(ADD_BOOKMARK);
+  const handleSubmit = () => {
+    console.log(titleField.value);
+    console.log(urlField.value);
+    addBookmark({
+      variables: {
+        url: urlField.value,
+        title: titleField.value,
+      },
+      refetchQueries: [{ query: GET_BOOKMARKS }],
+    });
+  };
 
-    const { error, loading, data } = useQuery(GET_BOOKMARKS);
-    const [addBookmark] = useMutation(ADD_BOOKMARK);
-    const handleSubmit = () => {
-        console.log(titleField.value)
-        console.log(urlField.value)
-        addBookmark({
-            variables: {
-                url: urlField.value,
-                title: titleField.value
-            },
-            refetchQueries: [{ query: GET_BOOKMARKS }]
-        })
-    }
+  if (error) return <h3>{error}</h3>;
 
-    if (error)
-        return <h3>{error}</h3>
+  if (loading) return <h3>Loading..</h3>;
 
-    if (loading)
-        return <h3>Loading..</h3>
-
-    return <div className="container">
-
+  return (
+    <div className="container-plus">
+      <div className="container">
         <h2>Add New Bookmark</h2>
         <label>
-            Enter Bookmark Tite: <br />
-            <input type="text" ref={node => titleField = node} />
+          Enter Bookmark Tite: <br />
+          <input type="text" ref={(node) => (titleField = node)} />
         </label>
 
         <br />
         <label>
-            Enter Bookmark Url: <br />
-            <input type="text" ref={node => urlField = node} />
+          Enter Bookmark Url: <br />
+          <input type="text" ref={(node) => (urlField = node)} />
         </label>
 
         <br />
@@ -69,8 +67,11 @@ export default function Home() {
         {/* {JSON.stringify(data.bookmarks)} */}
 
         <div className="card-container">
-            {data.bookmarks.map((bm) => <Card url={bm.url} title={bm.title} />)}
+          {data.bookmarks.map((bm) => (
+            <Card url={bm.url} title={bm.title} />
+          ))}
         </div>
-
+      </div>
     </div>
+  );
 }
