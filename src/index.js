ed96614a140942319ba673a5cc94f1769867c19b
Rao Akif <akifrao@gmail.com>
2021-02-03 18:42:09 +0500

    NextNode Template

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import Modal from "react-modal";
+
+Modal.setAppElement("#root");
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <App />,
+  document.getElementById("root")
 );
