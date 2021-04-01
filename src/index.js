8d235cf4035d61862b0283aa1891f7a095a0f793
Rao Akif <akifrao@gmail.com>
2021-04-01 18:22:37 +0500

    React ToDo

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import Modal from "react-modal";
-
-Modal.setAppElement("#root");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
 ReactDOM.render(
-  <App />,
-  document.getElementById("root")
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
